c83b9c82ee6af8e91c9195022d8a974d86dbd057
Aji Palar <ajikarunia@gmail.com>
2023-06-27 16:31:05 -0700

    towards writing table 2

--- one_five.py
+++ one_five.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pandas as pd
 import json
 
 def pp_aln(aln):
@@ -44,16 +45,72 @@ def parse_hhr_file(filename):
             alignments.append((header, meta, query, template))
     return alignments
 
+
+#test_hhr = parse_hhr_file("hhblits_out/A0FGR8.hhr")
+
+def parse_aln(aln):
+    header, meta, query, template = aln
+
+    meta_parts = meta.split()
+    meta_dict = {meta_part.split("=")[0]: meta_part.split("=")[1] for meta_part in meta_parts}
+    expected_keys = {'Probab', 'E-value', 'Score', 'Aligned_cols', 'Identities', 'Similarity', 'Sum_probs', 'Template_Neff'}
+    assert set(meta_dict.keys()) == expected_keys
+
+    return meta_dict
+
 hhrs = [i for i in Path("hhblits_out").iterdir() if i.suffix == ".hhr"]
-
-test_hhr = parse_hhr_file("hhblits_out/A0FGR8.hhr")
-
+assert len(hhrs) > 1
 if __name__ == "__main__":
-    hhr_alns = {}
     for hhr_file in hhrs:
+        QueryUID = []
+        PDB70ID = []
+        Probab = []
+        Evalue = []
+        Score = []
+        Aligned_cols = []
+        Identities = []
+        Similarity = []
+        Sum_probs = []
+        Template_Neff = []
+        Q = []
+        T = [] 
+
         alns = parse_hhr_file(str(hhr_file))
+        assert len(alns) > 0, hhr_file
         for aln in alns: 
-            pp_aln(aln)
-        hhr_alns[hhr_file.name] = parse_hhr_file(str(hhr_file))
-    
-#    json.dump(hhr_alns, open("hhblits_out/hhr_alns.json", "w"))
+            QueryUID.append(hhr_file.name.removesuffix(".hhr"))
+            assert len(aln) == 4
+            header, meta, query, template = aln
+            assert len(query) == len(template)
+
+            meta_parts = meta.split()
+            meta_dict = {meta_part.split("=")[0]: meta_part.split("=")[1] for meta_part in meta_parts}
+            expected_keys = {'Probab', 'E-value', 'Score', 'Aligned_cols', 'Identities', 'Similarity', 'Sum_probs', 'Template_Neff'}
+            assert set(meta_dict.keys()) == expected_keys
+
+            header_parts = header.split()
+            PDB70ID.append(header_parts[0].strip(">"))
+            Probab.append(meta_dict['Probab'])
+            Evalue.append(meta_dict['E-value'])
+            Score.append(meta_dict['Score'])
+            Aligned_cols.append(meta_dict['Aligned_cols'])
+            Identities.append(meta_dict['Identities'])
+            Similarity.append(meta_dict['Similarity'])
+            Sum_probs.append(meta_dict['Sum_probs'])
+            Template_Neff.append(meta_dict['Template_Neff'])
+            Q.append(query)
+            T.append(template)
+
+    df = pd.DataFrame({"QueryUID" : QueryUID,
+            "PDB70ID": PDB70ID,
+            "Probab": Probab,
+            "Evalue": Evalue,
+            "Score": Score,
+            "Aligned_cols": Aligned_cols,
+            "Identities": Identities,
+            "Similarity": Similarity,
+            "Sum_probs": Sum_probs,
+            "Template_Neff": Template_Neff,
+            "Q": Q,
+            "T": T})
+    df.to_csv("hhblits_out/hhr_pair_align.csv", index=False)
