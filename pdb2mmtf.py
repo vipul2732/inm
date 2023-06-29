5f9ed2c0ea984e1cd376e9b148e6ac6c7851a8a6
Aji Palar <ajikarunia@gmail.com>
2023-06-29 13:00:25 -0700

    convert cif to mmtf

--- pdb2mmtf.py
+++ pdb2mmtf.py
@@ -5,35 +5,44 @@ from pathlib import Path
 import click
 import biotite.structure.io.mmtf as mmtf
 import biotite.structure.io.pdb as pdb
+import biotite.structure.io.pdbx as pdbx
 
 
 @click.command()
 @click.option("-i", required=True, help="pdb file path")
 @click.option("-o", required=True, help="output dir")
-@click.option("-from", default="pdb")
+@click.option("-from", "from_", default="pdb")
 @click.option("-to", default="mmtf")
 def main(i, o, from_, to):
     if from_ == "pdb": 
         Reader = pdb.PDBFile
-	read_suffix = ".pdb"
-    
+        read_suffix = ".pdb"
+        read_mod = pdb
+    elif from_ == "cif":
+        Reader = pdbx.PDBxFile
+        read_suffix = ".cif"
+        read_mod = pdbx
+
     if to == "mmtf":
         write_mod = mmtf
         write_suffix = "mmtf"
         Writer = mmtf.MMTFFile
 
+
     pdb_file_path = i
     output_dir = o
 
-    db_file = pdb.PDBFile.read(pdb_file_path)
-    struct = db_file.get_structure()
+    db_file = Reader.read(pdb_file_path)
+    stack = read_mod.get_structure(db_file)
+    assert len(stack) == 1, f"multiple models in {pdb_file_path}"
+    array = stack[0]
 
-    pdb_id = Path(pdb_file_path).name.removesuffix(".pdb")
-    output_path = f"{output_dir}/{pdb_id}.mmtf"
+    pdb_id = Path(pdb_file_path).name.removesuffix(read_suffix)
+    output_path = f"{output_dir}/{pdb_id}.{write_suffix}"
     
-    mmtf_file = mmtf.MMTFFile()
-    mmtf.set_structure(mmtf_file, struct)
-    mmtf_file.write(output_path)
+    write_file = Writer()
+    write_mod.set_structure(write_file, array)
+    write_file.write(output_path)
 
 if __name__ == "__main__":
     main()
