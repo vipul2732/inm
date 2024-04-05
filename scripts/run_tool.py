0698038c650d73e1e72fb8fdfa47a4a3c62cfe32
aji <ajikarunia@gmail.com>
2024-04-04 17:56:15 -0700

    run_tool records the run configuration and outputs to modeling directory

--- scripts/run_tool.py
+++ scripts/run_tool.py
@@ -1,11 +1,17 @@
 import click
 import _run
 import run_configurations as rc
+from pathlib import Path
+import json
 
 @click.command()
 @click.argument("name")
 def main(name):
-    _run.main(**rc.__dict__[name]._asdict())
+    run_configuration =rc.__dict__[name]._asdict() 
+    jsonpath = Path(run_configuration['model_output_dirpath'] / "run_config.json")
+    with open(str(jsonpath), "w") as f:
+        json.dump(run_configuration, f)
+    _run.main(**run_configuration)
 
 if __name__ == "__main__":
     main()
