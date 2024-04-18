3cc5bdecf9b9428384cf18e8c63087823d106e46
aji <ajikarunia@gmail.com>
2024-04-18 15:37:15 -0700

    add --figures arguemnt

--- scripts/run_tool.py
+++ scripts/run_tool.py
@@ -4,14 +4,20 @@ import run_configurations as rc
 from pathlib import Path
 import json
 
+
 @click.command()
 @click.argument("name")
-def main(name):
+@click.option("--figures", default = False, is_flag = True)
+def main(name, figures):
     run_configuration =rc.__dict__[name]._asdict() 
-    jsonpath = Path(run_configuration['model_output_dirpath'] / "run_config.json")
-    with open(str(jsonpath), "w") as f:
-        json.dump(run_configuration, f)
-    _run.main(**run_configuration)
+    model_output_dirpath = Path(run_configuration['model_output_dirpath'])
+    if not figures: 
+        _run.main(**run_configuration)
+        jsonpath = model_output_dirpath  / "run_config.json"
+        with open(str(jsonpath), "w") as f:
+            json.dump(run_configuration, f)
+    else:
+        _run.figures(**run_configuration)
 
 if __name__ == "__main__":
     main()
