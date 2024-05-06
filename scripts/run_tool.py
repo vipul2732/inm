fd96dfd76cff3eae4587a4d8f9d7eebfe17d79de
aji <ajikarunia@gmail.com>
2024-05-06 16:20:22 -0700

    dynamically change rseed

--- scripts/run_tool.py
+++ scripts/run_tool.py
@@ -8,8 +8,13 @@ import json
 @click.command()
 @click.argument("name") 
 @click.option("--figures", default = False, is_flag = True, help="only run figures")
-def main(name, figures):
+@click.option("--rseed", default = None)
+def main(name, figures, rseed):
     run_configuration =rc.__dict__[name]._asdict() 
+    if rseed is not None:
+        rseed = int(rseed)
+        # update the run configuration dynamically
+        update_rc_dict_rseed(run_configuration, rseed)
     model_output_dirpath = Path(run_configuration['model_output_dirpath'])
     if not figures: 
         _run.main(**run_configuration)
@@ -18,6 +23,10 @@ def main(name, figures):
             json.dump(run_configuration, f)
     else:
         _run.figures(**run_configuration)
+
+def update_rc_dict_rseed(rc_dict, rseed):
+    rc_dict["rseed"] = rseed
+    rc_dict["model_output_dirpath"] = rc_dict["model_output_dirpath"] + f"_rseed_{rseed}"
 
 if __name__ == "__main__":
     main()
