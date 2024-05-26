672b344d588e724cf803f4a3696b947f7d99d7eb
aji <ajikarunia@gmail.com>
2024-05-25 18:59:40 -0700

    development on chain merging

--- scripts/run_tool.py
+++ scripts/run_tool.py
@@ -8,6 +8,7 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 from jax.tree_util import tree_map, tree_flatten, tree_unflatten
+import pandas as pd
 from pathlib import Path
 
 import run_configurations as rc
@@ -15,6 +16,13 @@ import _run
 sys.path.append("../notebooks")
 from sampling_assessment import (
         get_results
+)
+
+from data_io import (
+    pklload,
+    find_hmc_warmup_samples_file_path,
+    find_results_file_path,
+    find_model_data_file_path,
 )
 
 
@@ -62,7 +70,7 @@ def do_merge(name):
     ) 
     logging.info(f"merging {name} chains")
     base = Path(f"../results/{name}")
-    logging.info(f"base: {base}")
+    logging.info(f"base_path: {base}")
     #print("base", base)
 
     assert "rseed" not in base.name, "merge should exclude '_rseed_x'"
@@ -70,6 +78,7 @@ def do_merge(name):
     # make the output directory or overwrite
     if not out.is_dir():
         out.mkdir()
+    logging.info(f"out_path: {out}")
     
     merge_object = MergeObject(results=None, model_data=None) 
     merge_object = update_merge_object(name, merge_object, base)
@@ -78,31 +87,15 @@ def do_merge(name):
 def update_merge_object(name, merge_object, base):
     for path in base.parent.iterdir():
         if (path.is_dir()) and ("rseed" in path.name) and ("merge" not in path.name) and (name in path.name):
-            merge_object = update_merge_object_from_chain_path(name, merge_object, path)
+            rseed = int(path.name.split("_rseed_")[1])
+            merge_object = update_merge_object_from_chain_path(name, rseed, merge_object, path)
     return merge_object
 
-def update_merge_object_from_chain_path(name, merge_object, chain_path):
+def update_merge_object_from_chain_path(name, rseed, merge_object, chain_path):
     merge_object = update_merge_object_warmup_hmc(merge_object, chain_path)
-    merge_object = update_merge_object_results(name, merge_object, chain_path)
+    merge_object = update_merge_object_results(name, rseed, merge_object, chain_path)
     merge_object = update_merge_object_model_data(merge_object, chain_path)
     return merge_object
-
-def find_hmc_warmup_file_path(chain_path):
-    for path in chain_path.iterdir():
-        if path.is_file() and ("hmc_warmup" in path.name):
-            return path
-
-def find_results_file_path(name, chain_path):
-    pkl_files = [path for path in chain_path.iterdir() if (path.is_file()) and (".pkl" == path.suffix)]
-    pkl_files = [path for path in pkl_files if name in path.name] 
-    results_file = [path for path in pkl_files if (("hmc_warmup" not in path.name) and ("model_data" not in path.name))] 
-    assert len(results_file) == 1, f"results file not found in {chain_path}"
-    results_file_path = results_file[0]
-    return results_file_path
-
-def pklload(path):
-    with open(path, "rb") as f:
-        return pkl.load(f)
 
 def dictionary_expand_dims(dictionary) -> dict:
     return tree_map(lambda x: jnp.expand_dims(x, axis=0), dictionary)
@@ -111,8 +104,8 @@ def dictionary_concat_chains(x: dict, y: dict) -> dict:
     return tree_map(lambda a, b: jnp.concatenate([a, b], axis=0), x, y)
 
 def update_merge_object_warmup_hmc(merge_object, chain_path):
-    warmup_hmc_path = find_hmc_warmup_file_path(chain_path) 
-    print(warmup_hmc_path)
+    warmup_hmc_path = find_hmc_warmup_samples_file_path(chain_path) 
+    logging.info(f"updating merge object with warmup hmc from {warmup_hmc_path}")
     hmc_warmup = dictionary_expand_dims(pklload(warmup_hmc_path))
     if merge_object.hmc_warmup is None:
         merge_object.hmc_warmup = hmc_warmup 
@@ -120,13 +113,37 @@ def update_merge_object_warmup_hmc(merge_object, chain_path):
         merge_object.hmc_warmup = dictionary_concat_chains(merge_object.hmc_warmup, hmc_warmup)
     return merge_object
 
-def update_merge_object_results(name, merge_object, chain_path):
-    results_path = find_results_file_path(name, chain_path)
+def update_merge_object_results(name, rseed, merge_object, chain_path):
+    results_path = find_results_file_path(name, rseed, chain_path)
+    logging.info(f"updating merge object with results from {results_path}")
     results = dictionary_expand_dims(pklload(results_path))
     if merge_object.results is None:
         merge_object.results = results
     else:
         merge_object.results = dictionary_concat_chains(merge_object.results, results)
+    return merge_object
+
+def update_merge_object_model_data(merge_object, chain_path):
+    model_data_path = find_model_data_file_path(chain_path) 
+    logging.info(f"updating merge object with model_data from {model_data_path}")
+    model_data = pklload(model_data_path)
+    if merge_object.model_data is None:
+        merge_object.model_data = model_data
+    else:
+        for key in model_data.keys():
+            assert key in merge_object.model_data.keys(), "model_data should be the same"
+            try:
+                assert hash(model_data[key]) == hash(merge_object.model_data[key])
+            except TypeError:
+                logging.debug(f"except TypeError: chain path{chain_path}")
+                a = model_data[key]
+                b = merge_object.model_data[key]
+                logging.debug(f"key: {key}")
+                if isinstance(a, pd.DataFrame):
+                     logging.debug(f"a index {a.index}")
+                     logging.debug(f"b index {b.index}")
+                assert np.all(a == b), key 
+
     return merge_object
 
 def update_rc_dict_rseed(rc_dict, rseed):
