438f5febe2d66c69e81af4bbbede7c6dddf2a617
aji <ajikarunia@gmail.com>
2024-06-12 00:24:27 -0700

    attempt fix

--- scripts/auto_wynton.sh
+++ scripts/auto_wynton.sh
@@ -8,8 +8,8 @@ while true; do
 	qsub qsub_run_tool_high_mem.sh $MODEL1
 	qsub qsub_run_tool_high_mem.sh $MODEL2
 
-	tar -czvf ../results/{$MODEL1}_merged/figs300.tar.gz ../results/{$MODEL1}_merged/*300.png
-	tar -czvf ../results/{$MODEL2}_merged/figs300.tar.gz ../results/{$MODEL2}_merged/*300.png
+	tar -czvf ../results/"$MODEL1"_merged/figs300.tar.gz ../results/"$MODEL1"_merged/*300.png
+	tar -czvf ../results/"$MODEL2"_merged/figs300.tar.gz ../results/"$MODEL2"_merged/*300.png
 
 	sleep 300
 done
